1197c450744767e192a10ee21265e04dc9f159f0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2021-12-28 12:03:54 +0100

    added more runtime type checking

--- index.mjs
+++ index.mjs
@@ -1,7 +1,21 @@
-export const sequence = (promises) => {
-  return promises.reduce((previousPromise, currentPromise) => {
+export const sequence = (callbacks) => {
+  if (!Array.isArray(callbacks)) {
+    return Promise.reject(new TypeError("callbacks should be an array in sequence(callbacks)"));
+  }
+
+  if (!callbacks.every(promise => typeof promise === "function")) {
+    return Promise.reject(new TypeError("callbacks should be an array of functions in sequence(callbacks)"));
+  }
+
+  return callbacks.reduce((previousPromise, callback) => {
     return previousPromise.then(previousState => {
-      return currentPromise(previousState).then(newState => {
+      const callbackReturnValue = callback(previousState);
+
+      if (!(callbackReturnValue instanceof Promise)) {
+        return Promise.reject(new TypeError("callbacks should be an array of functions returning a promise in sequence(callbacks)"));
+      }
+
+      return callbackReturnValue.then(newState => {
         return [
           ...previousState,
           newState
