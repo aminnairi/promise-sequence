aec904cbcaa33b441672dfe4abb09e5ba297a54a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2021-12-28 12:20:19 +0100

    Added more comments (#15)

--- index.mjs
+++ index.mjs
@@ -1,20 +1,27 @@
 export const sequence = (callbacks) => {
+  // If the first argument is not an array
   if (!Array.isArray(callbacks)) {
     return Promise.reject(new TypeError("callbacks should be an array in sequence(callbacks)"));
   }
 
-  if (!callbacks.every(promise => typeof promise === "function")) {
+  // If the first argument is not an array of functions
+  if (!callbacks.every(callback => typeof callback === "function")) {
     return Promise.reject(new TypeError("callbacks should be an array of functions in sequence(callbacks)"));
   }
 
+  // For each callback
   return callbacks.reduce((previousPromise, callback) => {
+    // Resolve the previous state of previous reduce iteration
     return previousPromise.then(previousState => {
+      // Store the return value of the callback, called with the previous state
       const callbackReturnValue = callback(previousState);
 
+      // If the callback does not return a promise
       if (!(callbackReturnValue instanceof Promise)) {
         return Promise.reject(new TypeError("callbacks should be an array of functions returning a promise in sequence(callbacks)"));
       }
 
+      // Return the aggregation of the previous state and the next state of the promise
       return callbackReturnValue.then(newState => {
         return [
           ...previousState,
--- makefile
+++ makefile
@@ -1,19 +1,26 @@
+# Rules that prevent conflicting with other similar files or folders
 .PHONY: install test build publish
 
+# Base options when running docker-compose
 DOCKER_COMPOSE_RUN_OPTIONS=--rm
 
 ifeq (${CI},true)
+	# Additional options when running docker-compose in GitHub Action
 	DOCKER_COMPOSE_RUN_OPTIONS=--rm --user root -T
 endif
 
+# Install the project's dependencies
 install:
 	docker-compose run $(DOCKER_COMPOSE_RUN_OPTIONS) npm install
 
+# Run the unit tests
 test:
 	docker-compose run $(DOCKER_COMPOSE_RUN_OPTIONS) npm test
 
+# Build the optimized library file
 build:
 	docker-compose run $(DOCKER_COMPOSE_RUN_OPTIONS) npm run build
 
+# Publish the new library version to the NPM registries
 publish:
 	docker-compose run $(DOCKER_COMPOSE_RUN_OPTIONS) npm publish --access public
