2ef547fba20e28be39f336b475e594cf8490d7a0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2021-12-28 12:18:26 +0100

    added more comments

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,25 @@
+// Library for reducing the bundle size
 import {terser} from "rollup-plugin-terser";
+
+// Library for using various filesystem's helpers
 import {resolve} from "path";
 
 export default {
+  // Entrypoint of our library
   input: resolve("index.mjs"),
+
+  // Plugins to apply to the entrypoint file
   plugins: [
+    // Reduce the bundle size by minifying the input
     terser()
   ],
+
+  // Output settings
   output: {
+    // Output file to create
     file: resolve("build", "index.mjs"),
+
+    // Format of the output, here ECMAScript Modules
     format: "esm"
   }
 };
